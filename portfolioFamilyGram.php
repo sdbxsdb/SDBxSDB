278d68cc313a9e4cb0ba9a62e182f586a547b4bb
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2021-03-11 21:53:21 +0000

    Update portfolioFamilyGram.php

--- portfolioFamilyGram.php
+++ portfolioFamilyGram.php
@@ -6,7 +6,7 @@
   <div class="container">
     <div id="header">
       <div id="title">
-        <h1>Family Gram</h1>
+        <h1>FamGram</h1>
       </div>
       <div id="text">
         <p>With all the lockdowns and not being able to see family I came up with the idea of creating this website to allow my family to stay up to date with each other. It's an Instagram style platform I built using PHP Laravel. I used the command line to install and use:
