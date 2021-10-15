32aed00e45b607a10e4864fde251a5111032a088
Sam Donaghy-Bell <sam@reflex-studios.com>
2021-10-15 17:36:22 +0100

    updating skills order

--- home.php
+++ home.php
@@ -35,19 +35,19 @@
 
       <div id="aboutMeText" class="col-12 darkModeText">
         <p>I’m a former film & TV drama camera engineer of 12 years turned self-taught developer. <br><br>
-        
-        <h2>TL;DR</h2> This is my personal site where I'll share my skills, thoughts and ambitions. <br><br> I currently work as a Jr. Dev at Reflex Studios in Belfast.    I started at Reflex with 8 months of coding experience and since then I've been able to grow my skillset exponentially.  <br><br>
-        
-        I’m an extremely driven person and a fast learner with a "challenge accepted" approach to anything I don't know how to do.   I find I learn new skills most efficiently when coding real world solutions for real world problems.    <br><br> 
-        
+
+        <h2>TL;DR</h2> This is my personal site where I'll share my skills, thoughts and ambitions. <br><br> I currently work as a Jr. Dev at Reflex Studios in Belfast.    I started at Reflex with 8 months of coding experience and since then I've been able to grow my skillset exponentially. <br><br>
+
+        I’m an extremely driven person and a fast learner with a "challenge accepted" approach to anything I don't know how to do.   I find I learn new skills most efficiently when coding real world solutions for real world problems. <br><br>
+
         I also code for fun and for personal projects.   For example, when I first saw a <a id="mirrorIntroLink" href="/smartMirror.php" class="darkModeText">“smart mirror”</a> many years ago I knew that I wanted one build one of my own.  So I did.
-          <br><br>
+        <br><br>
 
-          <br><br>
-          I love a challenge, mental or otherwise and firmly believe it’s a sad day when you don’t learn something new.   Because of this I’m always looking to expand my knowledge in languages and tools etc (both back and front end) while refining the skills I already have to get as close to perfection as possible. <br><br>My goal is to become proficient in both front and back end development culminating in becoming a full stack developer.
+        <br><br>
+        I love a challenge, mental or otherwise and firmly believe it’s a sad day when you don’t learn something new.   Because of this I’m always looking to expand my knowledge in languages and tools etc (both back and front end) while refining the skills I already have to get as close to perfection as possible. <br><br>My goal is to become proficient in both front and back end development culminating in becoming a full stack developer.
 
-          <br><br>
-          If you have any questions or would like to get in touch I'd love to hear from you!
+        <br><br>
+        If you have any questions or would like to get in touch I'd love to hear from you!
         </p>
         <div id="aboutBounceBtn" class="arrow bounce"><i class="fa fa-angle-down fa-3x" aria-hidden="true"></i></div>
         <!-- <div>
@@ -79,42 +79,98 @@
     <div class="card darkModeBody darkModeText">
       <div class="card-body">
         <div class="skillsLogo">
-          <img src="bootstrapLogo.png" alt="" class="" width="100px">
-          <img src="tailwindLogo.png" alt="" class="" width="100px">
-        </div>
-        
-        <p class="card-text skillsText">I built my first website using zero bootstrap, my second using Bootstrap3, the third using BS4.  <br><br>I'm now very fond of <strong>Tailwind CSS</strong>. <br> It's small, highly and easily customisable and makes getting off the ground really fast.  <br><br> In recent projects I've taken to extending the life out of the themes so as to really unlock it's capabilities.  <br><br> I'm also familiar with the new JIT (just-in-time) mode.</p>
-      </div>
-    </div>
-
-    <div class="card darkModeBody darkModeText">
-      <div class="card-body">
-        <div class="skillsLogo">
-          <img src="figmaLogo.png" alt="" class="" width="80px">
-          <img src="sketchLogo.png" alt="" class="" width="100px">
-        </div>
-        <p class="card-text skillsText">I have experience in translating a designers designs from various programs such as - <br><br>
-        - Figma <br>
-        - Sketch <br>
-        - Zeplin <br>
-        </p>
-      </div>
-    </div>
-  </div>
-
-  <div class="card-deck">
-    <div class="card darkModeBody darkModeText">
-      <div class="card-body">
-        <div class="skillsLogo">
           <img src="sassLogo.png" alt="" class="" width="80px">
           <img src="cssLogo.png" alt="" class="" width="80px">
         </div>
         <p class="card-text skillsText">
-          I'm well versed in vanilla CSS, SASS and SCSS.  I always try to reduce, reuse and recycle classes and variables where possible.  
-          <br><br>I'm very familiar with flexbox and :nth and I'm familiar with CSS grids as well. <br><br> I also really enjoy building custom CSS animations and hover effects.</p>
+          I'm well versed in vanilla CSS, SASS and SCSS. I always try to reduce, reuse and recycle classes and variables where possible.
+          <br><br>I'm very familiar with flexbox and :nth and I'm familiar with CSS grids as well. <br><br> I also really enjoy building custom CSS animations and hover effects.
+        </p>
         <p class="card-text"></p>
       </div>
     </div>
+
+    <div class="card darkModeBody darkModeText">
+      <div class="card-body">
+        <div class="skillsLogo">
+          <img src="bootstrapLogo.png" alt="" class="" width="100px">
+          <img src="tailwindLogo.png" alt="" class="" width="100px">
+        </div>
+
+        <p class="card-text skillsText">I built my first website using zero bootstrap, my second using Bootstrap3, the third using BS4.  <br><br>I'm now very fond of <strong>Tailwind CSS</strong>. <br> It's small, highly and easily customisable and makes getting off the ground really fast.  <br><br> In recent projects I've taken to extending the life out of the themes so as to really unlock it's capabilities. <br><br> I'm also familiar with the new JIT (just-in-time) mode.</p>
+      </div>
+    </div>
+
+
+  </div>
+
+  <div class="card-deck">
+
+
+    <div class="card darkModeBody darkModeText">
+      <div class="card-body">
+        <div class="skillsLogo">
+          <img src="jsLogo.png" alt="" class="" width="80px">
+          <img src="jqueryLogo.png" alt="" class="" width="100px">
+        </div>
+        <p class="card-text skillsText">
+          I  &#10084;   the literal endless capabilities of this language. <br><br>
+          I'm well versed in vanilla JS as well as jQuery and it's many plugins. Whether via CND or NPM. <br><br>
+          Some recent projects have required: <br><br>
+        <div class="skillsText">
+          <li>- Form submissions via AJAX</li><br>
+          <li>- Checking logged in user data against collections for allowing different products to be purchased</li><br>
+          <li>- Showing warning message depending on products status</li><br>
+          <li>- Integrating What3Words into GoogleMaps API to allow clients to swap their coordinates for 3 words instead</li><br>
+          <li>- Looping through YouTube videos, getting ID's and applying the embedded thumbnail of each to an img tag for each video</li>
+
+        </div>
+
+        </p>
+
+      </div>
+
+    </div>
+    <div class="card darkModeBody darkModeText">
+      <div class="card-body">
+        <div class="skillsLogo">
+          <img src="apiLogo.png" alt="" class="" width="110px">
+          <img src="postmanLogo.png" alt="" class="" width="100px">
+        </div>
+        <p class="card-text skillsText">Using PostMan and Javascript I've worked with and customised a number of free and paid for APIs for client and personal use.  <br><br>These include but aren't limited to:
+        <div class="skillsText">
+          <li>- Youtube API</li>
+          <li>- GoogleMaps API</li>
+          <li>- What3Words API</li>
+          <li>- Snipcart API</li>
+          <li>- Shopify API</li>
+          <li>- Live Weather APIs</li>
+          <li>- Spotify API</li>
+          <li>- LastFM API</li>
+          <li>- fInternational Space Station tracking API</li>
+        </div>
+        </p>
+      </div>
+    </div>
+    <div class="card darkModeBody darkModeText">
+      <div class="card-body">
+        <div class="skillsLogo">
+          <img src="snipcartLogo.png" alt="" class="" width="100px">
+          <img src="shopifyLogo.png" alt="" class="" width="100px">
+        </div>
+        <p class="card-text skillsText">
+          I have experience in integrating Shopify's API into a CMS and also with coding in it's templating language Liquid&#128167;. <br><br>
+          I've recently introduced myself to the world of Snipcart. I've found Snipcart to be an excellent and arguably more flexible alternative to Shopify. <br><br> Using Snipcarts API I'm able to allow the client to add and products in their CMS (rather than jumping back and forth between CMS and shop). <br><br>
+          Snipcart has also allowed me to give clients access to their customers and assign special privileges like trade customers with complete control.
+
+        </p>
+
+      </div>
+
+    </div>
+  </div>
+
+  <div class="card-deck">
     <div class="card darkModeBody darkModeText">
       <div class="card-body">
         <div class="skillsLogo">
@@ -124,38 +180,11 @@
         <p class="card-text skillsText">I'm familiar with using Git on the command line, GitHub Desktop and terminal within VS Code for push, pulls and commits. <br><br>This website and other client builds have webhooks attached for automatic pulls to the server when master branch is updated but I'm also familiar with SSH for server access. <br><br> I'm somewhat familiar with file up-loaders such as CyberDuck but usually prefer the above methods where appropriate.
 
         </p>
-        
+
       </div>
-      
-      
+
+
     </div>
-    <div class="card darkModeBody darkModeText">
-      <div class="card-body">
-        <div class="skillsLogo">
-          <img src="jsLogo.png" alt="" class="" width="80px">
-          <img src="jqueryLogo.png" alt="" class="" width="100px">
-        </div>
-        <p class="card-text skillsText">
-        I  &#10084;   the literal endless capabilities of this language. <br><br>
-          I'm well versed in vanilla JS as well as jQuery and it's many plugins.  Whether via CND or NPM. <br><br>
-          Some recent projects have required: <br><br> 
-          <div class="skillsText">
-            <li>- Form submissions via AJAX</li><br>
-            <li>- Checking logged in user data against collections for allowing different products to be purchased</li><br>
-            <li>- Showing warning message depending on products status</li><br>
-            <li>- Integrating What3Words into GoogleMaps API to allow clients to swap their coordinates for 3 words instead</li><br>
-            <li>- Looping through YouTube videos, getting ID's and applying the embedded thumbnail of each to an img tag for each video</li>
-
-          </div>
-
-        </p>
-        
-      </div>
-      
-    </div>
-  </div>
-
-  <div class="card-deck">
     <div class="card darkModeBody darkModeText">
       <div class="card-body">
         <div class="skillsLogo">
@@ -166,58 +195,37 @@
     </div>
     <div class="card darkModeBody darkModeText">
       <div class="card-body">
-      <!--<h5 class="card-title">CMS</h5>-->
         <div class="skillsLogo">
-          <img src="statamicLogo.png" alt="" class="" width="100px">
+          <img src="figmaLogo.png" alt="" class="" width="80px">
+          <img src="sketchLogo.png" alt="" class="" width="100px">
         </div>
-        <p class="card-text skillsText">Statamic CMS<br><br> I was introduced to Statamic a short while ago after only using Wordpress and over many months now I've found it far more modern, powerful and intuitive - not to mention it's built on-top of Laravel! <br><br>
-        I have experience in gaging a clients needs for their CMS and what they want to achieve when in their Control Panel.  <br><br>
-        Simply updating text, adding a new image to a gallery or creating a new blog post to something more in-depth like custom contact form creation or page building with elements from the site ‘componentised’ to be dropped in as they wish.
+        <p class="card-text skillsText">I have experience in translating a designers designs from various programs such as - <br><br>
+          - Figma <br>
+          - Sketch <br>
+          - Zeplin <br>
         </p>
-        
       </div>
-      
     </div>
-    <div class="card darkModeBody darkModeText">
-      <div class="card-body">
-        <div class="skillsLogo">
-          <img src="snipcartLogo.png" alt="" class="" width="100px">
-          <img src="shopifyLogo.png" alt="" class="" width="100px">
-        </div>
-        <p class="card-text skillsText">
-        I have experience in integrating Shopify's API into a CMS and also with coding in it's templating language Liquid&#128167;. <br><br>
-          I've recently introduced myself to the world of Snipcart.  I've found Snipcart to be an excellent and arguably more flexible alternative to Shopify. <br><br>  Using Snipcarts API I'm able to allow the client to add and products in their CMS (rather than jumping back and forth between CMS and shop). <br><br>
-          Snipcart has also allowed me to give clients access to their customers and assign special privileges like trade customers with complete control.
 
-        </p>
-        
-      </div>
-      
-    </div>
+
   </div>
 
   <div class="card-deck">
     <div class="card darkModeBody darkModeText">
       <div class="card-body">
+        <!--<h5 class="card-title">CMS</h5>-->
         <div class="skillsLogo">
-          <img src="apiLogo.png" alt="" class="" width="110px">
-          <img src="postmanLogo.png" alt="" class="" width="100px">
+          <img src="statamicLogo.png" alt="" class="" width="100px">
         </div>
-        <p class="card-text skillsText">Using PostMan and Javascript I've worked with and customised a number of free and paid for APIs for client and personal use.  <br><br>These include but aren't limited to:
-          <div class="skillsText">
-            <li>- Youtube API</li>
-            <li>- GoogleMaps API</li>
-            <li>- What3Words API</li>
-            <li>- Snipcart API</li>
-            <li>- Shopify API</li>
-            <li>- Live Weather APIs</li>
-            <li>- Spotify API</li>
-            <li>- LastFM API</li>
-            <li>- fInternational Space Station tracking API</li>
-          </div>
+        <p class="card-text skillsText">Statamic CMS<br><br> I was introduced to Statamic a short while ago after only using Wordpress and over many months now I've found it far more modern, powerful and intuitive - not to mention it's built on-top of Laravel! <br><br>
+          I have experience in gaging a clients needs for their CMS and what they want to achieve when in their Control Panel. <br><br>
+          Simply updating text, adding a new image to a gallery or creating a new blog post to something more in-depth like custom contact form creation or page building with elements from the site ‘componentised’ to be dropped in as they wish.
         </p>
+
       </div>
+
     </div>
+
     <div class="card darkModeBody darkModeText">
       <div class="card-body">
         <div class="skillsLogo">
@@ -227,9 +235,9 @@
           Because I've been working a lot with Statamic CMS which is built on top of Laravel I have a bit of experience of this and Blade. <br><br>
           I do want to increase my PHP skills moving forward and build on the Laravel knowledge I have. In my portfolio you can find an Instagram copy I made with Blade via a few tutorials.
         </p>
-        
+
       </div>
-      
+
     </div>
     <div class="card darkModeBody darkModeText">
       <div class="card-body">
@@ -238,7 +246,7 @@
           <img src="reactLogo.png" alt="" class="" width="100px">
         </div>
         <p class="card-text skillsText">In a few recent projects I've used VueJs to work with dynamically input user data, pass data from 'page' to 'page' and display accordingly. <br><br>
-        I know I've barely scratched the surface of Vue, React and the likes but what I've done so far has me extremely keen to keep exploring. <br><br> The more practical, real world applications I can get the chance to work on the better and I'll jump at any chance to learn as much of Vue and or React as possible.
+          I know I've barely scratched the surface of Vue, React and the likes but what I've done so far has me extremely keen to keep exploring. <br><br> The more practical, real world applications I can get the chance to work on the better and I'll jump at any chance to learn as much of Vue and or React as possible.
         </p>
       </div>
     </div>
