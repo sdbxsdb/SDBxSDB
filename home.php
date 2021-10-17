9a2b92c25a2cd274ab5ede49db4620f29bbb722b
Sam Donaghy-Bell <sam@reflex-studios.com>
2021-10-17 21:36:20 +0100

    text fixes

--- home.php
+++ home.php
@@ -36,7 +36,7 @@
       <div id="aboutMeText" class="col-12 themeColorText">
         <p>I’m a former film & TV drama camera engineer of 12 years turned self-taught developer. <br><br>
 
-        <h2>TL;DR</h2> This is my personal site where I'll share my skills, thoughts and ambitions. <br><br> 
+        <h2>TL;DR</h2> This is my personal site where I'll share my skills, thoughts and ambitions.<br><br>
         I currently work as a Jr. Dev in Belfast.    I started my current position with 8 months of coding experience and since then I've been able to grow my skillset exponentially. <br><br>
 
         I’m an extremely driven person and a fast learner with a "challenge accepted" approach to anything I don't know how to do.   I find I learn new skills most efficiently when coding real world solutions for real world problems. <br><br>
@@ -44,7 +44,7 @@
         I also code for fun and for personal projects.   For example, when I first saw a <a id="mirrorIntroLink" href="/smartMirror.php" class="themeColorText">“smart mirror”</a> many years ago I knew that I wanted one build one of my own.  So I did.
         <br><br>
 
-        <br><br>
+
         I love a challenge, mental or otherwise and firmly believe it’s a sad day when you don’t learn something new.   Because of this I’m always looking to expand my knowledge in languages and tools etc (both back and front end) while refining the skills I already have to get as close to perfection as possible. <br><br>My goal is to become proficient in both front and back end development culminating in becoming a full stack developer.
 
         <br><br>
