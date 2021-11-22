b1e1b4aeeb7ea47c0b5a1ce5dd2754123875ec1c
Sam Donaghy-Bell <sam@reflex-studios.com>
2021-11-22 16:10:02 +0000

    update about me

--- home.php
+++ home.php
@@ -46,9 +46,6 @@
   
           I’m an extremely driven person and a fast learner with a "challenge accepted" approach to anything I don't know how to do.   I find I learn new skills most efficiently when coding real world solutions for real world problems. <br><br>
   
-          I also code for fun and for personal projects.   For example, when I first saw a  many years ago I knew that I wanted to build one of my own.  So I did.
-          <br><br>
-  
   
           I love a challenge, mental or otherwise and firmly believe it’s a sad day when you don’t learn something new.   Because of this I’m always looking to expand my knowledge in languages and tools etc (both back and front end) while refining the skills I already have to get as close to perfection as possible. <br><br>My goal is to become proficient in both front and back end development culminating in becoming a full stack developer.
   
